f8778ea1a130305db3723a577dd4eb28ff3e3ba2
Sasi Sekhar Koganti <sasisekhar2002@gmail.com>
2025-02-09 20:45:57 +0530

    commit with https

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:'http://feedback.adup.live/api'
+    baseURL:`${import.meta.env.VITE_BASE_URL}`
 })
 
 api.interceptors.request.use(
